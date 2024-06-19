00637085f082657360c112f4f2b1aaffd519b951
Mmela Dyantyi <1762984@students.wits.ac.za>
2024-06-19 11:04:00 +0200

    icecreampalor not optimized

---
+++ challenges/Hackerrank/19June/ice_cream_palour.js
@@ -0,0 +1,39 @@
+/*
+Two friends like to pool their money and go to the ice cream parlor. 
+They always choose two distinct flavors and they spend all of their money.
+
+Given a list of prices for the flavors of ice cream, select the two that will cost all of the money they have.
+
+Example.  
+
+The two flavors that cost  and  meet the criteria. Using -based indexing, they are at indices  and .
+
+Function Description
+
+Complete the icecreamParlor function in the editor below.
+
+icecreamParlor has the following parameter(s):
+
+int m: the amount of money they have to spend
+int cost[n]: the cost of each flavor of ice cream
+Returns
+
+int[2]: the indices of the prices of the two flavors they buy, sorted ascending
+*/
+/*
+ * Complete the 'icecreamParlor' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER m
+ *  2. INTEGER_ARRAY arr
+ * URL: https://www.hackerrank.com/challenges/icecream-parlor/problem?isFullScreen=true
+ */
+
+function icecreamParlor(m, arr) {
+  // Write your code here
+    while()
+}
+const m = 6;
+const arr = [1, 3, 4, 5, 6];
+console.log(icecreamParlor(m, arr));
